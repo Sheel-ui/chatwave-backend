fe34490ae33d76dd5cdbd4f96ba464da3c66557e
Sheel Taskar <sheeltaskar@Sheels-MacBook-Pro.local>
2024-01-05 18:56:29 -0500

    Added controller and tests for retrieving posts

---
+++ src/features/post/controllers/getPost.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from 'express';
+import HTTP_STATUS from 'http-status-codes';
+import { IPostDocument } from '@post/interfaces/postInterface';
+import { PostCache } from '@service/redis/postCache';
+import { postService } from '@service/db/postService';
+
+const postCache: PostCache = new PostCache();
+const PAGE_SIZE = 10;
+
+export class Get {
+    public async posts(req: Request, res: Response): Promise<void> {
+        const { page } = req.params;
+        // Calculate skip and limit values based on the page and PAGE_SIZE
+        const skip: number = (parseInt(page) - 1) * PAGE_SIZE;
+        const limit: number = PAGE_SIZE * parseInt(page);
+        const newSkip: number = skip === 0 ? skip : skip + 1;
+        let posts: IPostDocument[] = [];
+        let totalPosts = 0;
+        const cachedPosts: IPostDocument[] = await postCache.getPostsFromCache('post', newSkip, limit);
+        // Check if there are cached posts
+        if (cachedPosts.length) {
+            posts = cachedPosts;
+            totalPosts = await postCache.getTotalPostsInCache();
+        } else {
+            // Retrieve posts from the service if no cached posts are available
+            posts = await postService.getPosts({}, skip, limit, { createdAt: -1 });
+            totalPosts = await postService.postsCount();
+        }
+        res.status(HTTP_STATUS.OK).json({ message: 'All posts', posts, totalPosts });
+    }
+
+    public async postsWithImages(req: Request, res: Response): Promise<void> {
+        const { page } = req.params;
+        const skip: number = (parseInt(page) - 1) * PAGE_SIZE;
+        const limit: number = PAGE_SIZE * parseInt(page);
+        const newSkip: number = skip === 0 ? skip : skip + 1;
+        let posts: IPostDocument[] = [];
+        const cachedPosts: IPostDocument[] = await postCache.getPostsWithImagesFromCache('post', newSkip, limit);
+        // Fetch posts with images from the service if not available in the cache
+        posts = cachedPosts.length
+            ? cachedPosts
+            : await postService.getPosts({ imgId: '$ne', gifUrl: '$ne' }, skip, limit, { createdAt: -1 });
+        res.status(HTTP_STATUS.OK).json({ message: 'All posts with images', posts });
+    }
+}
--- src/features/post/routes/postRoute.ts
+++ src/features/post/routes/postRoute.ts
@@ -1,6 +1,7 @@
 import express, { Router } from 'express';
 import { authMiddleware } from '@global/helpers/authMiddleware';
 import { Create } from '@post/controllers/createPost';
+import { Get } from '@post/controllers/getPost';
 
 class PostRoutes {
     private router: Router;
@@ -11,6 +12,9 @@ class PostRoutes {
 
     // Post routes
     public routes(): Router {
+        this.router.get('/post/all/:page', authMiddleware.checkAuthentication, Get.prototype.posts);
+        this.router.get('/post/images/:page', authMiddleware.checkAuthentication, Get.prototype.postsWithImages);
+
         this.router.post('/post', authMiddleware.checkAuthentication, Create.prototype.post);
         this.router.post('/post/image/post', authMiddleware.checkAuthentication, Create.prototype.post);
         return this.router;
