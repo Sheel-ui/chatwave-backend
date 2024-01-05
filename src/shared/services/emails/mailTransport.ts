e99b3601a8e47e414d0c2c222669ef447e791950
Sheel Taskar <sheeltaskar@Sheels-MacBook-Pro.local>
2024-01-04 19:38:22 -0500

    Added Mail Transporter

---
+++ src/shared/services/emails/mailTransport.ts
@@ -0,0 +1,76 @@
+import nodemailer from 'nodemailer';
+import Mail from 'nodemailer/lib/mailer';
+import Logger from 'bunyan';
+import sendGridMail from '@sendgrid/mail';
+import { config } from '@root/config';
+import { BadRequestError } from '@global/helpers/errorHandler';
+
+// Structure for email options
+interface IMailOptions {
+  from: string;
+  to: string;
+  subject: string;
+  html: string;
+}
+
+const log: Logger = config.createLogger('mailOptions');
+sendGridMail.setApiKey(config.SENDGRID_API_KEY!);
+
+class MailTransport {
+  public async sendEmail(receiverEmail: string, subject: string, body: string): Promise<void> {
+    // Check env and send email
+    if (config.NODE_ENV === 'test' || config.NODE_ENV === 'dev') {
+      this.developmentEmailSender(receiverEmail, subject, body);
+    } else {
+      this.productionEmailSender(receiverEmail, subject, body);
+    }
+  }
+
+  private async developmentEmailSender(receiverEmail: string, subject: string, body: string): Promise<void> {
+    // Nodemailer transporter for sending emails in development
+    const transporter: Mail = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      secure: false,
+      auth: {
+        user: config.SENDER_EMAIL!,
+        pass: config.SENDER_EMAIL_PASSWORD!
+      }
+    });
+
+    const mailOptions: IMailOptions = {
+      from: `Chatty App <${config.SENDER_EMAIL!}>`,
+      to: receiverEmail,
+      subject,
+      html: body
+    };
+
+    try {
+      await transporter.sendMail(mailOptions);
+      log.info('Development email sent successfully.');
+    } catch (error) {
+      log.error('Error sending email', error);
+      throw new BadRequestError('Error sending email');
+    }
+  }
+
+  private async productionEmailSender(receiverEmail: string, subject: string, body: string): Promise<void> {
+    // Nodemailer transporter for sending emails in production
+    const mailOptions: IMailOptions = {
+      from: `Chatty App <${config.SENDER_EMAIL!}>`,
+      to: receiverEmail,
+      subject,
+      html: body
+    };
+
+    try {
+      await sendGridMail.send(mailOptions);
+      log.info('Production email sent successfully.');
+    } catch (error) {
+      log.error('Error sending email', error);
+      throw new BadRequestError('Error sending email');
+    }
+  }
+}
+
+export const mailTransport: MailTransport = new MailTransport();
