73e607035e9457d43f0669a6a3ee0773144ce07a
Sheel Taskar <sheeltaskar@Sheels-MacBook-Pro.local>
2024-01-05 22:29:39 -0500

    Added remove reaction controller

--- src/features/reactions/routes/reactionRoutes.ts
+++ src/features/reactions/routes/reactionRoutes.ts
@@ -1,6 +1,7 @@
 import express, { Router } from 'express';
 import { authMiddleware } from '@global/helpers/authMiddleware';
 import { Add } from '@reaction/controllers/addReaction';
+import { Remove } from '@reaction/controllers/removeReaction';
 
 class ReactionRoutes {
     private router: Router;
@@ -12,6 +13,13 @@ class ReactionRoutes {
     public routes(): Router {
         // POST
         this.router.post('/post/reaction', authMiddleware.checkAuthentication, Add.prototype.reaction);
+
+        // DELETE
+        this.router.delete(
+            '/post/reaction/:postId/:previousReaction/:postReactions',
+            authMiddleware.checkAuthentication,
+            Remove.prototype.reaction
+        );
 
         return this.router;
     }
