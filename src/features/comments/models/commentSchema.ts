202580d530237dcf5e69eff0d85a15c03e8e10b3
Sheel Taskar <sheeltaskar@Sheels-MacBook-Pro.local>
2024-01-06 00:05:08 -0500

    Added methods to save and get comments in cache

--- src/features/comments/models/commentSchema.ts
+++ src/features/comments/models/commentSchema.ts
@@ -2,12 +2,12 @@ import mongoose, { model, Model, Schema } from 'mongoose';
 import { ICommentDocument } from '@comment/interfaces/commentInterface';
 
 const commentSchema: Schema = new Schema({
-  postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', index: true },
-  comment: { type: String, default: '' },
-  username: { type: String },
-  avataColor: { type: String },
-  profilePicture: { type: String },
-  createdAt: { type: Date, default: Date.now() }
+    postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', index: true },
+    comment: { type: String, default: '' },
+    username: { type: String },
+    avataColor: { type: String },
+    profilePicture: { type: String },
+    createdAt: { type: Date, default: Date.now() }
 });
 
 const CommentsModel: Model<ICommentDocument> = model<ICommentDocument>('Comment', commentSchema, 'Comment');
