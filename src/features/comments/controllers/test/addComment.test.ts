eb25cf35e0c1a66042efd8105ad4488f27d1a252
Sheel Taskar <sheeltaskar@Sheels-MacBook-Pro.local>
2024-01-06 12:07:44 -0500

    Added follower interface and schema

--- src/features/comments/controllers/test/addComment.test.ts
+++ src/features/comments/controllers/test/addComment.test.ts
@@ -11,52 +11,52 @@ jest.mock('@service/queues/baseQueue');
 jest.mock('@service/redis/commentCache');
 
 describe('Add', () => {
-  beforeEach(() => {
-    jest.restoreAllMocks();
-  });
-
-  afterEach(() => {
-    jest.clearAllMocks();
-    jest.clearAllTimers();
-  });
-
-  it('should call savePostCommentToCache and addCommentJob methods', async () => {
-    const req: Request = reactionMockRequest(
-      {},
-      {
-        postId: '6027f77087c9d9ccb1555268',
-        comment: 'This is a comment',
-        profilePicture: 'https://place-hold.it/500x500',
-        userTo: `${existingUser._id}`
-      },
-      authUserPayload
-    ) as Request;
-    const res: Response = reactionMockResponse();
-    jest.spyOn(CommentCache.prototype, 'savePostCommentToCache');
-    jest.spyOn(commentQueue, 'addCommentJob');
-
-    await Add.prototype.comment(req, res);
-    expect(CommentCache.prototype.savePostCommentToCache).toHaveBeenCalled();
-    expect(commentQueue.addCommentJob).toHaveBeenCalled();
-  });
-
-  it('should send correct json response', async () => {
-    const req: Request = reactionMockRequest(
-      {},
-      {
-        postId: '6027f77087c9d9ccb1555268',
-        comment: 'This is a comment',
-        profilePicture: 'https://place-hold.it/500x500',
-        userTo: `${existingUser._id}`
-      },
-      authUserPayload
-    ) as Request;
-    const res: Response = reactionMockResponse();
-
-    await Add.prototype.comment(req, res);
-    expect(res.status).toHaveBeenCalledWith(200);
-    expect(res.json).toHaveBeenCalledWith({
-      message: 'Comment created successfully'
+    beforeEach(() => {
+        jest.restoreAllMocks();
     });
-  });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+        jest.clearAllTimers();
+    });
+
+    it('should call savePostCommentToCache and addCommentJob methods', async () => {
+        const req: Request = reactionMockRequest(
+            {},
+            {
+                postId: '6027f77087c9d9ccb1555268',
+                comment: 'This is a comment',
+                profilePicture: 'https://place-hold.it/500x500',
+                userTo: `${existingUser._id}`
+            },
+            authUserPayload
+        ) as Request;
+        const res: Response = reactionMockResponse();
+        jest.spyOn(CommentCache.prototype, 'savePostCommentToCache');
+        jest.spyOn(commentQueue, 'addCommentJob');
+
+        await Add.prototype.comment(req, res);
+        expect(CommentCache.prototype.savePostCommentToCache).toHaveBeenCalled();
+        expect(commentQueue.addCommentJob).toHaveBeenCalled();
+    });
+
+    it('should send correct json response', async () => {
+        const req: Request = reactionMockRequest(
+            {},
+            {
+                postId: '6027f77087c9d9ccb1555268',
+                comment: 'This is a comment',
+                profilePicture: 'https://place-hold.it/500x500',
+                userTo: `${existingUser._id}`
+            },
+            authUserPayload
+        ) as Request;
+        const res: Response = reactionMockResponse();
+
+        await Add.prototype.comment(req, res);
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({
+            message: 'Comment created successfully'
+        });
+    });
 });
