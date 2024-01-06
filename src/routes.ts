33352b4066fba21af5efb43d0d0297da40cd6dd9
Sheel Taskar <sheeltaskar@Sheels-MacBook-Pro.local>
2024-01-06 00:31:16 -0500

    Added comment routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { authRoutes } from '@auth/routes/authRoutes';
 import { currentUserRoutes } from '@auth/routes/currentRoutes';
+import { commentRoutes } from '@comment/routes/commentRoutes';
 import { authMiddleware } from '@global/helpers/authMiddleware';
 import { postRoutes } from '@post/routes/postRoute';
 import { reactionRoutes } from '@reaction/routes/reactionRoutes';
@@ -17,6 +18,7 @@ export default (app: Application) => {
         app.use(BASE_PATH, authMiddleware.verifyUser, currentUserRoutes.routes());
         app.use(BASE_PATH, authMiddleware.verifyUser, postRoutes.routes());
         app.use(BASE_PATH, authMiddleware.verifyUser, reactionRoutes.routes());
+        app.use(BASE_PATH, authMiddleware.verifyUser, commentRoutes.routes());
     };
     routes();
 };
