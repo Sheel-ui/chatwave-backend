b5b48b6f7af4b028d8120ea0da1793b518d3ddc4
Sheel Taskar <sheeltaskar64@gmail.com>
2024-07-03 17:18:55 +0530

    add get follower route

--- src/features/followers/routes/followerRoute.ts
+++ src/features/followers/routes/followerRoute.ts
@@ -1,6 +1,7 @@
 import express, { Router } from 'express';
 import { authMiddleware } from '@global/helpers/authMiddleware';
 import { Add } from '@follower/controllers/followUser';
+import { Get } from '@follower/controllers/getFollowers';
 import { Remove } from '@follower/controllers/unfollowUser';
 
 class FollowerRoutes {
@@ -11,6 +12,10 @@ class FollowerRoutes {
     }
 
     public routes(): Router {
+
+        this.router.get('/user/following', authMiddleware.checkAuthentication, Get.prototype.userFollowing);
+        this.router.get('/user/followers/:userId', authMiddleware.checkAuthentication, Get.prototype.userFollowers);
+
         // PUT
         this.router.put('/user/follow/:followerId', authMiddleware.checkAuthentication, Add.prototype.follower);
         this.router.put('/user/unfollow/:followeeId/:followerId', authMiddleware.checkAuthentication, Remove.prototype.follower);
