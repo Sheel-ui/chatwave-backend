853cf525fa3cbf920985211aae38f1bd62bb6dff
Sheel Taskar <56334666+Sheel-ui@users.noreply.github.com>
2024-07-05 17:48:40 +0530

    video upload feature (#10)

--- src/features/post/routes/postRoute.ts
+++ src/features/post/routes/postRoute.ts
@@ -17,14 +17,17 @@ class PostRoutes {
         // GET
         this.router.get('/post/all/:page', authMiddleware.checkAuthentication, Get.prototype.posts);
         this.router.get('/post/images/:page', authMiddleware.checkAuthentication, Get.prototype.postsWithImages);
+        this.router.get('/post/videos/:page', authMiddleware.checkAuthentication, Get.prototype.postsWithVideos);
 
         // POST
         this.router.post('/post', authMiddleware.checkAuthentication, Create.prototype.post);
         this.router.post('/post/image/post', authMiddleware.checkAuthentication, Create.prototype.postWithImage);
+        this.router.post('/post/video/post', authMiddleware.checkAuthentication, Create.prototype.postWithVideo);
 
         // UPDATE
         this.router.put('/post/:postId', authMiddleware.checkAuthentication, Update.prototype.posts);
         this.router.put('/post/image/:postId', authMiddleware.checkAuthentication, Update.prototype.postWithImage);
+        this.router.put('/post/video/:postId', authMiddleware.checkAuthentication, Update.prototype.postWithVideo);
 
         // DELETE
         this.router.delete('/post/:postId', authMiddleware.checkAuthentication, Delete.prototype.post);
